d565e226188f459847cfb42f856c679f7791fa10
Coossy <charles-emile.cossette@polymtl.ca>
2022-05-10 16:01:49 -0400

    exercices2

--- exercice.py
+++ exercice.py
@@ -24,12 +24,12 @@ def to_degrees(angle_rads: float) -> tuple:
     return 0.0, 0.0, 0.0
 
 
-def to_celsius(temperature: float) -> float:
-    return 0.0
+def to_celsius(temperature: float) -> float: 
+    return((temperature - 32)/ 1.8)
 
 
-def to_farenheit(temperature: float) -> float:
-    return 0.0
+def to_farenheit(temperature: float) -> float: 
+    return(temperature * 1.8 + 32)
 
 
 def main() -> None:
